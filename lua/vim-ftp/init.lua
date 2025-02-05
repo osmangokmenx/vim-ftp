66c008833a3845064c4c65d6430b3748ceb91805
jengal-osman-gokmen <osman.gokmen@jengal.com>
2025-02-05 11:20:23 +0300

    add progress bar

--- lua/vim-ftp/init.lua
+++ lua/vim-ftp/init.lua
@@ -24,6 +24,10 @@ local function get_relative_path(filepath)
     return filepath:gsub(cwd, "")
 end
 
+local function notify(msg, type)
+    vim.notify(msg, type or vim.log.levels.INFO)
+end
+
 function M.upload()
     local filepath = vim.fn.expand('%:p')
     local rel_path = get_relative_path(filepath)
@@ -31,11 +35,22 @@ function M.upload()
     select_server(function(server)
         local remote_path = server.root .. rel_path
         local cmd = string.format(
-            "curl -T '%s' ftp://%s%s --user %s:%s",
+            "curl -T '%s' ftp://%s%s --user %s:%s --progress-bar",
             filepath, server.host, remote_path, server.user, server.password
         )
-        vim.fn.system(cmd)
-        print("Uploaded to " .. remote_path)
+        
+        vim.fn.jobstart(cmd, {
+            on_stdout = function(_, data)
+                if data then notify(table.concat(data, '\n')) end
+            end,
+            on_exit = function(_, code)
+                if code == 0 then
+                    notify("Uploaded to " .. remote_path, vim.log.levels.INFO)
+                else
+                    notify("Upload failed!", vim.log.levels.ERROR)
+                end
+            end
+        })
     end)
 end
 
@@ -46,11 +61,22 @@ function M.download()
     select_server(function(server)
         local remote_path = server.root .. rel_path
         local cmd = string.format(
-            "curl -o '%s' ftp://%s%s --user %s:%s",
+            "curl -o '%s' ftp://%s%s --user %s:%s --progress-bar",
             filepath, server.host, remote_path, server.user, server.password
         )
-        vim.fn.system(cmd)
-        print("Downloaded from " .. remote_path)
+        
+        vim.fn.jobstart(cmd, {
+            on_stdout = function(_, data)
+                if data then notify(table.concat(data, '\n')) end
+            end,
+            on_exit = function(_, code)
+                if code == 0 then
+                    notify("Downloaded from " .. remote_path, vim.log.levels.INFO)
+                else
+                    notify("Download failed!", vim.log.levels.ERROR)
+                end
+            end
+        })
     end)
 end
 
